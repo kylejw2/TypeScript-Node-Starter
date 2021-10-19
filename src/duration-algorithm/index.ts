2d37ada3c6b2b861905ebfd624822665eeb94411
Kyle Wright <kylejamwright@gmail.com>
2021-10-19 14:06:34 -0600

    study graphs and trees for next week

--- src/duration-algorithm/index.ts
+++ src/duration-algorithm/index.ts
@@ -1,11 +1,23 @@
-export interface TemplateType {
+export interface TaskType {
   duration: number
   id: number
   dependency: number
 }
 
-const durationAlgorithm = (templates: TemplateType[]): number => {
+const durationAlgorithm = (tasks: TaskType[]): number => {
   const duration = 0;
+  const visited = [];
+  const dependencies: any = {};
+  tasks.map(task => {
+    dependencies[task.dependency] = [...dependencies[task.dependency], task.id];
+  });
+  const base = tasks.filter(task => task.dependency !== null);
+
+  tasks.forEach(task => {
+
+  });
+
+
 
   return duration;
 };
